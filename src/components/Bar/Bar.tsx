3a20632acb7e9324db3e0059cb6d522cc8c475d4
Julia <mjulia1794@gmail.com>
2021-09-22 14:48:22 +0300

    add bar component

---
+++ src/components/Bar/Bar.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react'
+import './bar.css'
+
+
+export default function Bar(): ReactElement {
+  return (
+    <div className="bar">
+      <div className="bar_logo">
+        <img src="/image/logo.png" className="logo_img" alt="logo" />
+      </div>
+      <div className="bar_contacts">
+        <span className="contacts_text">
+          Besoin d'aide ?
+        </span>
+        <a href="mailto:"><img className="contact_mail" alt="mail" /></a>
+        <a href="tel:"><img className="contact_phone" alt="phone" /></a>
+      </div>
+    </div>
+  )
+}
