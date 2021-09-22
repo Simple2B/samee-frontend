8c32106b2173d6a427b125c1523ed6a7da682416
Julia <mjulia1794@gmail.com>
2021-09-22 14:47:23 +0300

    add guarantees component

---
+++ src/components/Guarantees/Guarantees.tsx
@@ -0,0 +1,66 @@
+import React, { ReactElement } from 'react'
+import { useHistory } from 'react-router-dom'
+import Popup from 'reactjs-popup'
+import './guarantees.css'
+
+
+export default function Guarantees(): ReactElement {
+  const history = useHistory()
+
+  const handleSubmit = () => {
+    return history.push("/occupation")
+  }
+  return (
+    <div className="guarantees">
+      <div className="guarantees_title">Contrairement à une banque, une assurance peut vous proposer plus de garantie: </div>
+      <ol className="guarantees_list">
+        <li className="guarantees_list-item">s'occupe du paiement de vos primes en cas d'invalidité </li>
+        <li className="guarantees_list-item">permet l'obtention d'une rente complémentaire à votre salaire en cas d'invalidité</li>
+        <li className="guarantees_list-item">garantit la totalité du 3ème pilier que vous aurez touché à la retraite pour vos proches en cas de décès. </li>
+        <li className="guarantees_list-item"> offre la possibilité de ne pas payer la prime si une année est plus difficile. </li>
+      </ol>
+
+      <div className="guarantees_text">En revanche, la solution en banque est plus flexible, car vous n'êtes pas lié à un contrat de versement de primes.</div>
+
+      <button onClick={handleSubmit} className="next_button">Passer au calculateur</button>
+
+      <Popup modal trigger={<div className="pop_up_triger">Plus d'informations sur la différence</div>}>
+        {(close: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void) | undefined) => (
+          <>
+            <button className="close" onClick={close}>
+              X
+    </button>
+            <div className="pop_up">
+              <div className="pop_up_title">Plus d'informations sur la différence entre banque et assurance</div>
+              <div className="pop_up_text">En choisissant un 3ème pilier en assurance, vous avez la possibilité, mais pas l’obligation, de bénéficier de ces avantages. Vous pouvez choisir uniquement certains avantages ou tous les avantages. Des frais seront déduits de votre rendement pour assurer ces prestations.
+</div>
+              <div className="pop_up_text">Concernant le point 1, la libération du paiement des primes en cas d’invalidité, la personne peut choisir les délais d’attente entre 3,6 ou 12 mois d’attente.
+</div>
+              <div className="pop_up_text">Si vous n’êtes plus en mesure de travailler suite à une maladie ou un accident, la compagnie d’assurance paiera vos primes jusqu’à l’âge de votre retraite.
+</div>
+              <div className="pop_up_text">Pour le point 2 , selon votre contrat, vous vous assurez un complément de rente afin d’être sûr d’obtenir 80% de votre salaire actuel au lieu des 60%- 70% couverts par le 1er et le 2ème pilier.
+</div>
+              <div className="pop_up_text">Concernant le point 3, vous pouvez garantir à vos héritiers l’intégralité du montant prévu. Il est impossible de choisir les bénéficiaires ou de modifier l’ordre légal car la loi applique la clause bénéficiaire prévue à cet effet.
+</div>
+              <div className="pop_up_text">Voici le détail de la clause bénéficiaires 3A :
+              Le conjoint ou partenaire enregistré (le concubin n’est pas considéré comme conjoint)
+              Les trois points suivants sont au même niveau
+              Les descendants directs (enfants, petits-enfants, …) ou personnes à l’entretien desquelles le défunt subvenait de façon prépondérante.
+              La personne qui avait formé avec le défunt une communauté de vie ininterrompue durant 5 ans minimum immédiatement avant le décès
+              La personne qui subvient à l’entretien d’enfants communs.
+              Les parents
+              Les frères et sœurs
+              Les autres héritiers
+              Le preneur d’assurance peut modifier l’ordre des trois derniers points et définir leurs droits.
+</div>
+              <div className="pop_up_text">Pour le point 4, toutes les compagnies ne le proposent pas, et en général vous pouvez suspendre votre prime durant un ou deux ans non consécutifs.
+</div>
+              <div className="pop_up_text">
+                Avec un contrat d’assurance, vous êtes obligé de verser une prime chaque année, ce qui peut être un désavantage comme un avantage, car vous épargnerez pour votre futur et éviterez certaines dépenses plus futiles.</div>
+            </div>
+          </>)}
+      </Popup>
+
+    </div>
+  )
+}
