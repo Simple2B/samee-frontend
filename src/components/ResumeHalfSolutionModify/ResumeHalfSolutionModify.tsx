6a8160e3d90fe267cf031baaa61ad409d5c2cb93
Julia <mjulia1794@gmail.com>
2021-10-28 16:52:14 +0300

    responsive

--- src/components/ResumeHalfSolutionModify/ResumeHalfSolutionModify.tsx
+++ src/components/ResumeHalfSolutionModify/ResumeHalfSolutionModify.tsx
@@ -8,7 +8,7 @@ import {useEffect} from 'react';
 const useStyles = makeStyles({
   root: {
     color: 'white !important',
-    fontSize: '24px !important',
+    fontSize: '1em !important',
     fontFamily: '"Archivo Narrow" !important',
     borderBottom: '1px solid white !important',
   },
@@ -107,93 +107,97 @@ export default function ResumeHalfSolutionModify(): ReactElement {
 
   return (
     <div className="modify_parameters">
-      <div className="modify_parameters_text">
-        Vous pouvez essayer avec un autre montant d'épargne
-        <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
-          <Select
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-standard"
-            value={period}
-            onChange={handlePeriod}
-            label="salary"
-            className={classes.root}
-            classes={{
-              nativeInput: classes.nativeInput,
-            }}
-            inputProps={{
-              classes: {
-                icon: classes.icon,
-              },
-            }}>
-            <MenuItem value="mensuel">mensuel</MenuItem>
-            <MenuItem value="annuel">annuel</MenuItem>
-          </Select>
-        </FormControl>
-        {period === 'mensuel' ? (
-          <input
-            min="100"
-            max="573"
-            onChange={handleSalary}
-            value={salaryFromLocal}
-            type="number"
-            className="employee_salary"
-          />
-        ) : (
-          <input
-            min="1200"
-            max="6883"
-            onChange={handleSalary}
-            value={salaryFromLocal}
-            type="number"
-            className="employee_salary"
-          />
+      <div className="main_content">
+        <div className="modify_parameters_text">
+          Vous pouvez essayer avec un autre montant d'épargne
+          <FormControl variant="standard" sx={{m: 1, minWidth: 80}}>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              value={period}
+              onChange={handlePeriod}
+              label="salary"
+              className={classes.root}
+              classes={{
+                nativeInput: classes.nativeInput,
+              }}
+              inputProps={{
+                classes: {
+                  icon: classes.icon,
+                },
+              }}>
+              <MenuItem value="mensuel">mensuel</MenuItem>
+              <MenuItem value="annuel">annuel</MenuItem>
+            </Select>
+          </FormControl>
+          {period === 'mensuel' ? (
+            <input
+              min="100"
+              max="573"
+              onChange={handleSalary}
+              value={salaryFromLocal}
+              type="number"
+              className="employee_salary"
+            />
+          ) : (
+            <input
+              min="1200"
+              max="6883"
+              onChange={handleSalary}
+              value={salaryFromLocal}
+              type="number"
+              className="employee_salary"
+            />
+          )}
+        </div>
+
+        <div className="modify_parameters_text">
+          et avec une répartition différente
+        </div>
+
+        <div className="half-optimal-proportion_percents">
+          <div className="proportion_percent left">
+            <div className="percent gold_text">{`${sliderValue}%`}</div>
+            <div className="percent-desc">Epargne</div>
+          </div>
+
+          <div className="circle_button">
+            <CircleSlider
+              size={260}
+              min={10}
+              max={90}
+              progressWidth={10}
+              circleWidth={10}
+              progressColor={'white'}
+              stepSize={10}
+              value={sliderValue}
+              onChange={handleSliderChange}
+            />
+
+            <div className="circle"></div>
+          </div>
+
+          <div className="proportion_percent right">
+            <div className="percent gold_text">{`${100 - sliderValue}%`}</div>
+            <div className="percent-desc">Fonds</div>
+          </div>
+        </div>
+
+        {error && (
+          <div className="error_block_position">
+            <img src="/image/error.png" className="error_img" alt="error" />
+            {error}{' '}
+          </div>
         )}
       </div>
 
-      <div className="modify_parameters_text">
-        et avec une répartition différente
-      </div>
-
-      <div className="half-optimal-proportion_percents">
-        <div className="proportion_percent">
-          <div className="percent gold_text">{`${sliderValue}%`}</div>
-          <div className="percent-desc">Epargne</div>
+      <div className="footer_content">
+        <div className="buttons_set button_position">
+          <div className="error">{errorAmount}</div>
+          <button onClick={handleRecalculate} className="next_button ">
+            Recalculer
+          </button>
         </div>
-
-        <div className="circle_button">
-          <CircleSlider
-            size={260}
-            min={10}
-            max={90}
-            progressWidth={10}
-            circleWidth={10}
-            progressColor={'white'}
-            stepSize={10}
-            value={sliderValue}
-            onChange={handleSliderChange}
-          />
-
-          <div className="circle"></div>
-        </div>
-
-        <div className="proportion_percent">
-          <div className="percent gold_text">{`${100 - sliderValue}%`}</div>
-          <div className="percent-desc">Fonds</div>
-        </div>
-      </div>
-
-      {error && (
-        <div className="error_block_position">
-          <img src="/image/error.png" className="error_img" alt="error" />
-          {error}{' '}
-        </div>
-      )}
-
-      <div className="buttons_set button_position">
-        <div className="error">{errorAmount}</div>
-        <button onClick={handleRecalculate} className="next_button ">
-          Recalculer
-        </button>
       </div>
     </div>
   );
