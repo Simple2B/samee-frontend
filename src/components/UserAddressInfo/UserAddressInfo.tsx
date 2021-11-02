c3334eb59eefa590e7b626daad80c05d23095290
Julia <mjulia1794@gmail.com>
2021-11-02 08:27:51 +0200

    formik

--- src/components/UserAddressInfo/UserAddressInfo.tsx
+++ src/components/UserAddressInfo/UserAddressInfo.tsx
@@ -3,19 +3,44 @@ import classNames from 'classnames';
 import {useState} from 'react';
 import Geosuggest from 'react-geosuggest';
 import {useHistory} from 'react-router-dom';
+import {Formik} from 'formik';
 import Popup from 'reactjs-popup';
 import {ProgressContext} from '../../context/progressContext';
 import './userAddressInfo.css';
 
+type Nullable<T> = T | null | undefined;
+
+interface IAddressInfo {
+  name: string;
+  lastName: string;
+  number: string;
+  postcode: string;
+  city: string;
+  street: string;
+}
+
+const initialValues = {
+  name: '',
+  lastName: '',
+  number: '',
+  postcode: '',
+  city: '',
+  street: '',
+};
+
 export default function UserAddressInfo(): ReactElement {
-  const [name, setName] = useState();
-  const [lastName, setLastName] = useState();
-  const [number, setNumber] = useState();
-  const [postcode, setPostcode] = useState();
-  const [city, setCity] = useState();
-  const [street, setStreet] = useState();
+  // const [name, setName] = useState();
+  // const [lastName, setLastName] = useState();
+  // const [number, setNumber] = useState();
+  // const [postcode, setPostcode] = useState();
+  // const [city, setCity] = useState();
+  // const [street, setStreet] = useState();
+
   const [error, setError] = useState('');
-  const [inputError, setInputError] = useState(false);
+  // const [inputError, setInputError] = useState(false);
+  const [formValues, setFormValues] = useState<IAddressInfo>(initialValues);
+  const [formErrors, setFormErrors] = useState<Nullable<IAddressInfo>>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const history = useHistory();
 
@@ -25,50 +50,92 @@ export default function UserAddressInfo(): ReactElement {
     setProgress(26);
   }, []);
 
-  const addressData = {
-    name: name,
-    lastName: lastName,
-    number: number,
-    postcode: postcode,
-    city: city,
-    street: street,
+  const submitForm = (values: IAddressInfo) => {
+    setError('');
+    localStorage.setItem('userAddressData', JSON.stringify(values));
+    history.push('/user-personal-info');
   };
 
-  const handleName = (e: any) => {
-    const clearedValue = e.target.value.replace(/[0-9]/g, '');
-    setName(clearedValue);
-    setInputError(false);
-  };
+  const validate = (values: IAddressInfo) => {
+    let errors = {} as IAddressInfo;
 
-  const handleLastName = (e: any) => {
-    const clearedValue = e.target.value.replace(/[0-9]/g, '');
-    setLastName(clearedValue);
-  };
-
-  const handleStreet = (e: any) => {
-    console.log(e);
-    if (e) {
-      const clearedValue = e.description;
-      setStreet(clearedValue);
+    if (!values.name) {
+      errors.name = 'required';
     }
-  };
-
-  const handleNumber = (e: any) => {
-    const clearedValue = e.target.value.replace(/\D/g, '');
-    setNumber(clearedValue);
-  };
-
-  const handlePostcode = (e: any) => {
-    const clearedValue = e.target.value.replace(/\D/g, '');
-    setPostcode(clearedValue);
-  };
-
-  const handleCity = (e: any) => {
-    if (e) {
-      const clearedValue = e.description;
-      setCity(clearedValue);
+    if (!values.lastName) {
+      errors.lastName = 'required';
     }
+
+    if (!values.number) {
+      errors.number = 'required';
+    }
+
+    if (!values.postcode) {
+      errors.postcode = 'required';
+    }
+    if (!values.city) {
+      errors.city = 'required';
+    }
+    if (!values.street) {
+      errors.street = 'required';
+    }
+    if (
+      !values.name ||
+      !values.lastName ||
+      !values.number ||
+      !values.postcode ||
+      !values.city ||
+      !values.street
+    ) {
+      setError('Veuillez renseigner toutes les informations');
+    }
+
+    return errors;
   };
+
+  // const addressData = {
+  //   name: name,
+  //   lastName: lastName,
+  //   number: number,
+  //   postcode: postcode,
+  //   city: city,
+  //   street: street,
+  // };
+
+  // const handleName = (e: any) => {
+  //   const clearedValue = e.target.value.replace(/[0-9]/g, '');
+  //   setName(clearedValue);
+  // };
+
+  // const handleLastName = (e: any) => {
+  //   const clearedValue = e.target.value.replace(/[0-9]/g, '');
+  //   setLastName(clearedValue);
+  // };
+
+  // const handleStreet = (e: any) => {
+  //   console.log(e);
+  //   if (e) {
+  //     const clearedValue = e.description;
+  //     setStreet(clearedValue);
+  //   }
+  // };
+
+  // const handleNumber = (e: any) => {
+  //   const clearedValue = e.target.value.replace(/\D/g, '');
+  //   setNumber(clearedValue);
+  // };
+
+  // const handlePostcode = (e: any) => {
+  //   const clearedValue = e.target.value.replace(/\D/g, '');
+  //   setPostcode(clearedValue);
+  // };
+
+  // const handleCity = (e: any) => {
+  //   if (e) {
+  //     const clearedValue = e.description;
+  //     setCity(clearedValue);
+  //   }
+  // };
 
   const onKeyPress = (event: any) => {
     console.log(event);
@@ -82,18 +149,18 @@ export default function UserAddressInfo(): ReactElement {
     }
   };
 
-  const handleSubmit = (e: {preventDefault: () => void}) => {
-    if (!name || !lastName || !number || !postcode || !city || !street) {
-      e.preventDefault();
-      setError('veuillez renseigner toutes les informations');
-      setInputError(true);
-    } else {
-      setError('');
-      setInputError(false);
-      localStorage.setItem('userAddressData', JSON.stringify(addressData));
-      history.push('/user-personal-info');
-    }
-  };
+  // const handleSubmit = (e: {preventDefault: () => void}) => {
+  //   if (!name || !lastName || !number || !postcode || !city || !street) {
+  //     e.preventDefault();
+  //     setError('veuillez renseigner toutes les informations');
+  //     setInputError(true);
+  //   } else {
+  //     setError('');
+  //     setInputError(false);
+  //     localStorage.setItem('userAddressData', JSON.stringify(addressData));
+  //     history.push('/user-personal-info');
+  //   }
+  // };
 
   return (
     <div className="user_address_info">
@@ -104,106 +171,154 @@ export default function UserAddressInfo(): ReactElement {
           besoins de quelques informations supplémentaires.
         </div>
 
-        <div className="user_address_info_input_block">
-          <div className="user_address_info_input-left">
-            <div className="user_address_info_input-set">
-              <label htmlFor="name" className="input_label">
-                Nom
-              </label>
-              <input
-                value={name}
-                onChange={handleName}
-                name="name"
-                className={classNames('input_field', {
-                  'input_field error_input': inputError,
-                  input_field: name,
-                })}
-              />
-            </div>
-            <div className="user_address_info_input-set">
-              <label htmlFor="street" className="input_label">
-                Rue
-              </label>
-              <Geosuggest
-                value={street}
-                onChange={handleStreet}
-                inputClassName={classNames('input_field', {
-                  'input_field error_input': inputError,
-                })}
-                placeholder=""
-                country="CH"
-                onSuggestSelect={handleStreet}
-                suggestsHiddenClassName="hidden"
-                types={['geocode']}
-              />
-            </div>
-            <div className="user_address_info_input-set">
-              <label htmlFor="postcode" className="input_label">
-                NPA
-              </label>
-              <input
-                maxLength={4}
-                value={postcode}
-                onChange={handlePostcode}
-                name="postcode"
-                className={classNames('input_field', {
-                  'input_field error_input': inputError,
-                })}
-              />
-            </div>
-          </div>
-          <div className="user_address_info_input-right">
-            <div className="user_address_info_input-set">
-              <label htmlFor="lastname" className="input_label">
-                Prénom
-              </label>
-              <input
-                value={lastName}
-                onChange={handleLastName}
-                name="lastname"
-                className={classNames('input_field', {
-                  'input_field error_input': inputError,
-                })}
-              />
-            </div>
-            <div className="user_address_info_input-set">
-              <label htmlFor="number" className="input_label">
-                Numéro
-              </label>
-              <input
-                value={number}
-                onChange={handleNumber}
-                name="number"
-                className={classNames('input_field', {
-                  'input_field error_input': inputError,
-                })}
-              />
-            </div>
-            <div className="user_address_info_input-set">
-              <label htmlFor="city" className="input_label">
-                Ville
-              </label>
-              <Geosuggest
-                value={city}
-                inputClassName={classNames('input_field', {
-                  'input_field error_input': inputError,
-                })}
-                placeholder=""
-                country="CH"
-                onSuggestSelect={handleCity}
-                suggestsHiddenClassName="hidden"
-                onKeyPress={event => onKeyPress(event)}
-                types={['(cities)']}
-              />
-            </div>
-          </div>
-        </div>
+        <Formik
+          initialValues={initialValues}
+          validate={validate}
+          onSubmit={submitForm}>
+          {formik => {
+            const {
+              values,
+              handleSubmit,
+              handleChange,
+              errors,
+              touched,
+              handleBlur,
+              isValid,
+              dirty,
+            } = formik;
+            return (
+              <form
+                id="form"
+                onSubmit={values => handleSubmit(values)}
+                className="user_address_info_input_block">
+                <div className="user_address_info_input-left">
+                  <div className="user_address_info_input-set">
+                    <label htmlFor="name" className="input_label">
+                      Nom
+                    </label>
+                    <input
+                      value={values.name.replace(/[0-9]/g, '')}
+                      onChange={handleChange}
+                      name="name"
+                      className={
+                        errors.name && touched.name
+                          ? 'input_field error_input'
+                          : 'input_field'
+                      }
+                    />
+                  </div>
+                  <div className="user_address_info_input-set">
+                    <label htmlFor="street" className="input_label">
+                      Rue
+                    </label>
+                    <Geosuggest
+                      value={values.street}
+                      onChange={handleChange}
+                      name="street"
+                      inputClassName={
+                        errors.street && touched.street
+                          ? 'input_field error_input'
+                          : 'input_field'
+                      }
+                      placeholder=""
+                      country="CH"
+                      onSuggestSelect={(e: any) => {
+                        if (e) {
+                          values.street = e.description;
+                        }
+                      }}
+                      suggestsHiddenClassName="hidden"
+                      types={['geocode']}
+                    />
+                  </div>
+                  <div className="user_address_info_input-set">
+                    <label htmlFor="postcode" className="input_label">
+                      NPA
+                    </label>
+                    <input
+                      maxLength={4}
+                      value={values.postcode.replace(/\D/g, '')}
+                      onChange={handleChange}
+                      name="postcode"
+                      className={
+                        errors.postcode && touched.postcode
+                          ? 'input_field error_input'
+                          : 'input_field'
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="user_address_info_input-right">
+                  <div className="user_address_info_input-set">
+                    <label htmlFor="lastname" className="input_label">
+                      Prénom
+                    </label>
+                    <input
+                      value={values.lastName.replace(/[0-9]/g, '')}
+                      onChange={handleChange}
+                      name="lastName"
+                      className={
+                        errors.lastName && touched.lastName
+                          ? 'input_field error_input'
+                          : 'input_field'
+                      }
+                    />
+                  </div>
+                  <div className="user_address_info_input-set">
+                    <label htmlFor="number" className="input_label">
+                      Numéro
+                    </label>
+                    <input
+                      value={values.number.replace(/\D/g, '')}
+                      onChange={handleChange}
+                      name="number"
+                      className={
+                        errors.number && touched.number
+                          ? 'input_field error_input'
+                          : 'input_field'
+                      }
+                    />
+                  </div>
+                  <div className="user_address_info_input-set">
+                    <label htmlFor="city" className="input_label">
+                      Ville
+                    </label>
+                    <Geosuggest
+                      value={values.city.replace(/[0-9]/g, '')}
+                      onChange={handleChange}
+                      name="city"
+                      inputClassName={
+                        errors.city && touched.city
+                          ? 'input_field error_input'
+                          : 'input_field'
+                      }
+                      placeholder=""
+                      country="CH"
+                      onSuggestSelect={(e: any) => {
+                        if (e) {
+                          values.city = e.description;
+                        }
+                      }}
+                      suggestsHiddenClassName="hidden"
+                      onKeyPress={event => onKeyPress(event)}
+                      types={['(cities)']}
+                    />
+                  </div>
+                </div>
+              </form>
+            );
+          }}
+        </Formik>
       </div>
 
       <div className="footer_contant">
         <div className="button_set button_position">
           <div className="error">{error}</div>
-          <button onClick={handleSubmit} className="next_button">
+          <button
+            type="submit"
+            form="form"
+            // onClick={handleSubmit}
+            className="next_button">
             Continuer
           </button>
         </div>
