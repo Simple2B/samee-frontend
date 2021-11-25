38f70304b41e2c8539424b026c650300b5799e68
Julia <mjulia1794@gmail.com>
2021-11-25 18:04:03 +0200

    links

--- src/components/UserContactInfo/UserContactInfo.tsx
+++ src/components/UserContactInfo/UserContactInfo.tsx
@@ -253,6 +253,12 @@ export default function UserContactInfo(): ReactElement {
                 </div>
               </div>
 
+              <div className="links">
+                <a className="link" href="#">
+                  Some links
+                </a>
+              </div>
+
               <div className="user_contact_info_check_block">
                 <div className="choose_solution_list">
                   J'ai lu et j'accepte les conditions générales
