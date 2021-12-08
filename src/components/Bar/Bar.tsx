4c1c645e7b44de47b2d8ae3cc7cf3e3f096f12b6
Julia <mjulia1794@gmail.com>
2021-12-08 11:02:51 +0200

    add whatsapp

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -14,8 +14,12 @@ export default function Bar(): ReactElement {
         <a href="mailto:contact@samee.ch">
           <img src="/image/mail.png" className="contact_mail" alt="mail" />
         </a>
-        <a href="tel:+41782231082">
-          <img src="/image/phone.png" className="contact_phone" alt="phone" />
+        <a href=" https://wa.me/41782231082">
+          <img
+            src="/image/whatsapp.svg"
+            className="contact_phone"
+            alt="phone"
+          />
         </a>
       </div>
     </div>
